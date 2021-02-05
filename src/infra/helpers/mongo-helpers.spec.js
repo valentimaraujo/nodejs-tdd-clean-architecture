3b21b82d624a15ea2343c78329158fd45905441c
Valentim Araújo <bitbucket@evodev.com.br>
2021-02-05 19:38:53 -0300

    refactory

--- src/infra/helpers/mongo-helpers.spec.js
+++ src/infra/helpers/mongo-helpers.spec.js
@@ -1,8 +1,15 @@
 const MongoHelper = require('./mongo-helpers')
 
 describe('Mongo Help', () => {
-  test('Should an connect if mongo has disconnected', async () => {
+  beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should an connect if mongo has disconnected', async () => {
     await MongoHelper.disconnect()
     const db = await MongoHelper.getDb()
     expect(db).not.toBeNull()
